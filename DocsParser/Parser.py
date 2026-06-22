1f67c085a47cd7d7e2c6b132046b4db54cfafdf1
metriiics <metriics@yandex.ru>
2026-06-22 23:43:22 +1000

    rework router parsing

--- DocsParser/Parser.py
+++ DocsParser/Parser.py
@@ -11,10 +11,16 @@ class ParseDocs:
 
         self.converter = DocumentConverter()
 
+        self.parsers = {
+            ".docx": self.pars_docx_pdf, 
+            ".pdf": self.pars_docx_pdf,
+            ".xlsx": self.pars_xlsx
+        }
+
     def detect_files(self, name_dir: str) -> list:
         dir = self.abs_path / name_dir
         if not dir.is_dir() or not dir.exists():
-            return f"[ERROR]This directory not found or not directory. {dir}"
+            return f"Директория не найдена или не существует: {dir}"
 
         files = []
 
@@ -34,14 +40,19 @@ class ParseDocs:
             if not file.is_file():
                 continue
 
-            text = self.pars_docx_pdf(file)
+            parser = self.parsers.get(file.suffix.lower())
+            if parser is None:
+                print(f"Неизвестный формат: {file}")
+                continue
+
+            content = parser(file)
             doc = Documents(
                 name_docs=file.name,
-                content=text,
+                content=content,
                 metadata=DocMetadata(
                     file_type=file.suffix,
-                    length_char=len(text),
-                    length_word=len(text.split())
+                    length_char=len(content),
+                    length_word=len(content.split())
                 )
             )
             parse_content.append(doc)
@@ -55,5 +66,5 @@ class ParseDocs:
         
         return markdown
 
-    def pars_xlsx(self):
-        pass
+    def pars_xlsx(self, file):
+        return "XLSX File - Wow!"
